4a591b2e233b644ca4a1fcf6d33912717a45a313
ondy01 <kadleceo@arcig.cz>
2025-03-24 20:16:06 +0100

    Add files via upload

--- binary_heap.py
+++ binary_heap.py
@@ -1,41 +1,62 @@
 from dataclasses import dataclass
-from typing import Any
 
 @dataclass
 class Element:
-    value: Any
-    priority: int
- 
+    value = False
+    priority = 0
 
 class BinaryHeap:
-
     def __init__(self):
         self.heap = []
     
+    def _parent(self, index):
+        return (index - 1) // 2
+    
+    def _leva_odnoz(self, index):
+        return 2 * index + 1
+    
+    def _prava_odnoz(self, index):
+        return 2 * index + 2
+    
+    def _nahoru(self, index):
+        while index > 0 and self.heap[index].priority < self.heap[self._parent(index)].priority:
+            self.heap[index], self.heap[self._parent(index)] = self.heap[self._parent(index)], self.heap[index]
+            index = self._parent(index)
+    
+    def _dolu(self, index):
+        smallest = index
+        leva = self._leva_odnoz(index)
+        prava = self._prava_odnoz(index)
+        
+        if leva < len(self.heap) and self.heap[leva].priority < self.heap[smallest].priority:
+            smallest = leva
+        
+        if prava < len(self.heap) and self.heap[prava].priority < self.heap[smallest].priority:
+            smallest = prava
+        
+        if smallest != index:
+            self.heap[index], self.heap[smallest] = self.heap[smallest], self.heap[index]
+            self._dolu(smallest)
+    
     def push(self, element):
-        # naivni implementace binarni haldy
         self.heap.append(element)
-
+        self._nahoru(len(self.heap) - 1)
+    
     def pop(self):
-        # naivni implementace binarni haldy
-        # najdi nejmensi prvek a vrat ho
         if not self.heap:
             raise Exception("Heap is empty")
-        min_idx = 0
-        for idx in range(1,len(self.heap)):
-            if self.heap[idx].priority < self.heap[min_idx].priority:
-                min_idx = idx
-        return self.heap.pop(min_idx)
-
-
+        
+        root = self.heap[0]
+        self.heap[0] = self.heap[-1]
+        self.heap.pop()
+        
+        if self.heap:
+            self._dolu(0)
+        
+        return root
+    
     def head(self):
         if not self.heap:
             raise Exception("Heap is empty")
-        # vrati nejmensi element ve fronte (element na cele fronty)
-        # protoze mame naivni implementaci, musime projit cely seznam
-        min_idx = 0
-        for idx in range(1,len(self.heap)):
-            if self.heap[idx].priority < self.heap[min_idx].priority:
-                min_idx = idx
-        return self.heap[min_idx]
-        
+        return self.heap[0]
+
